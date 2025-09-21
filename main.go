2074b98dc95c88c854c2df093ebe6d84351538b9
Nanakusa <JonatanFD>
2025-09-21 18:58:46 -0500

    feat: add swagger

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	variables "code-runner/env"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files" // swagger embed files
+	ginSwagger "github.com/swaggo/gin-swagger"
+
+	_ "code-runner/docs"
 )
 
 func main() {
@@ -11,11 +16,17 @@ func main() {
 
 	router := gin.Default()
 
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
+	// Grupo con prefijo de API
+	api := router.Group("/api/" + variables.AppConfig.APIVersion)
+	{
+		api.GET("/ping", func(c *gin.Context) {
+			c.JSON(200, gin.H{"message": "pong"})
 		})
-	})
+	}
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	fmt.Println("Swagger UI disponible en: http://localhost:" + variables.AppConfig.Port + "/swagger/index.html")
 
 	router.Run(":" + variables.AppConfig.Port)
 }
