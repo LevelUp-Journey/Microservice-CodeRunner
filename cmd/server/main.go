097c91670d7b524a08f0f0f5bd476d5ddcef97bf
JonatanFD <118645187+JonatanFD@users.noreply.github.com>
2025-09-22 09:32:21 -0500

    feat: enhance API documentation and add solution execution handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,19 @@
+// @title Microservice CodeRunner API
+// @version 1.0
+// @description API para ejecutar código de soluciones de desafíos de programación
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8084
+// @BasePath /api/v1
+
+// @schemes http https
 package main
 
 import (
@@ -5,7 +21,6 @@ import (
 	"code-runner/internal/app"
 )
 
-// Entry point of the application
 func main() {
 	app.Run()
 }
