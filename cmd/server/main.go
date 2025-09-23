12b92761cd31c00a2e7b21691d8a060f5ea28f58
JonatanFD <118645187+JonatanFD@users.noreply.github.com>
2025-09-23 01:37:41 -0500

    fix: unused annotations

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,18 +1,5 @@
 // @title Microservice CodeRunner API
 // @version 1.0
-// @description API para ejecutar código de soluciones de desafíos de programación
-// @termsOfService http://swagger.io/terms/
-
-// @contact.name API Support
-// @contact.url http://www.swagger.io/support
-// @contact.email support@swagger.io
-
-// @license.name Apache 2.0
-// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
-
-// @host localhost:8084
-// @BasePath /api/v1
-
 // @schemes http https
 package main
 
